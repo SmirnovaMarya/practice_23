16fc6e602594911ef6bf58c7a8789102f36867b4
SmirnovaMarya <unlenmaria@gmail.com>
2026-03-27 22:28:19 +0300

    isEmpty bad implementation

--- L1/main.cpp
+++ L1/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <iomanip>
 #include "vector-top-it.hpp"
 
 int main()
 {
-  using namespace topit;
-  Vector <int> v1;
-  Vector<int> v2;
-  if (v1 == v2)
+  bool test1()
   {
-
+    using topit::Vector;
+    Vector < int > v;
+    return v.isEmpty;
+  }
+  using test_t = bool(*)();
+  test_t tests[] = {
+    test1
+  };
+  size_t count = sizeof(tests) / sizeof(tests_t);
+  std::cout << std::boolalpha;
+   for(size_t i = 0 ; i < ; ++i){
+    std::cout << tests[i]() << ":" << i << "\n";
   }
 }
