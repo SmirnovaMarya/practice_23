67689ec9f96efd89fa9b64ff6a746f729eb24fc5
SmirnovaMarya <unlenmaria@gmail.com>
2026-03-27 22:19:18 +0300

    vector POD

---
+++ L1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "vector-top-it.hpp"
+
+int main()
+{
+  using namespace topit;
+  Vector <int> v1;
+  Vector<int> v2;
+  if (v1 == v2)
+  {
+
+  }
+}
---
+++ L1/vector-top-it.hpp
@@ -0,0 +1,17 @@
+#ifndef VECTOR_TOP_IT_HPP
+#define VECTOR_TOP_IT_HPP
+
+#include <cstddef>
+namespace topit
+{
+template < class T >
+  struct Vector
+  {
+    T *data;
+    size_t size, capacity;
+  };
+  template < class T >
+  bool operator==(const Vector < T > & lhs, const Vector < T >  & rhs);
+}
+
+#endif
